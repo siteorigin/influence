083f3f40bb0d81d5b02f792e6e0fcaca56176199
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-05-06 04:09:53 +0000

    Added PageNavi support. Minor CSS changes.

--- themes/influence/trunk/inc/template-tags.php
+++ themes/influence/trunk/inc/template-tags.php
@@ -37,7 +37,9 @@ function influence_content_nav( $nav_id ) {
 	<nav role="navigation" id="<?php echo esc_attr( $nav_id ); ?>" class="<?php echo $nav_class; ?>">
 		<h1 class="assistive-text"><?php _e( 'Post navigation', 'influence' ); ?></h1>
 
-		<?php if ( is_single() ) : // navigation links for single posts ?>
+		<?php if ( function_exists( 'wp_pagenavi' ) ) : wp_pagenavi( ); ?>
+
+		<?php elseif ( is_single() ) : // navigation links for single posts ?>
 
 			<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'influence' ) . '</span> %title' ); ?>
 			<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'influence' ) . '</span>' ); ?>
