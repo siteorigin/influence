c141735a717a53e9304593a9d2e82db89a6514b5
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-08-20 21:33:16 +0000

    Tagged Influence

---
+++ themes/influence/tags/1.0.2/demo/slider.php
@@ -0,0 +1,48 @@
+<div class="sow-slider-base" style="display: none">
+
+	<ul class="sow-slider-images" data-settings="{&quot;pagination&quot;:true,&quot;speed&quot;:850,&quot;timeout&quot;:5000}">
+		<li class="sow-slider-image sow-slider-image-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/demo/assets/slide-2.jpg);">
+
+			<div class="sow-slider-image-container">
+				<div class="sow-slider-image-wrapper" style="max-width: 720px;">
+					<img width="720" height="500" src="<?php echo get_template_directory_uri() ?>/demo/assets/slider-badge.png" class="attachment-full" alt="slider-badge">
+				</div>
+			</div>
+
+		</li>
+
+		<li class="sow-slider-image sow-slider-image-cover" style="background-image: url();">
+
+			<div class="sow-slider-image-container">
+				<div class="sow-slider-image-wrapper" style="max-width: 700px;">
+					<img width="700" height="500" src="<?php echo get_template_directory_uri() ?>/demo/assets/slider-badge-2.png" class="attachment-full" alt="slider-badge-2">
+				</div>
+			</div>
+
+			<video class="sow-background-element" autoplay="" loop="" muted="">
+				<source src="<?php echo get_template_directory_uri() ?>/demo/assets/writing-video.mp4" type="video/mp4">
+				<source src="<?php echo get_template_directory_uri() ?>/demo/assets/writing-video.ogv" type="video/ogg">
+			</video>
+
+		</li>
+
+	</ul>
+
+	<ol class="sow-slider-pagination">
+		<li><a href="#" data-goto="0">1</a></li>
+		<li><a href="#" data-goto="1">2</a></li>
+	</ol>
+
+	<div class="sow-slide-nav sow-slide-nav-next">
+		<a href="#" data-goto="next" data-action="next">
+			<em class="icon-thin-right"></em>
+		</a>
+	</div>
+
+	<div class="sow-slide-nav sow-slide-nav-prev">
+		<a href="#" data-goto="previous" data-action="prev">
+			<em class="icon-thin-left"></em>
+		</a>
+	</div>
+
+</div>
